5f3566459394102d6823bcc03d9c0e90f1fab700
Agus <agusgus612@gmail.com>
2025-12-19 01:12:31 +0700

    add isi barang data

--- latihan1/Data/BarangData.cs
+++ latihan1/Data/BarangData.cs
@@ -1,6 +1,14 @@
-﻿namespace latihan1.Data
+﻿using latihan1.Models;
+
+namespace latihan1.Data
 {
-    public class BarangData
+    public static class BarangData
     {
+        public static List<Barang> ListBarang = new()
+        {
+            new Barang { Id = 1, Kode = "K001", Nama = "Sabun Lifebouy", Guadang= "G001", Stok= 30 },
+            new Barang { Id = 2, Kode = "K002", Nama = "Sunlight Jeruk Nipis Pencuci piring", Guadang = "G002", Stok = 12},
+            new Barang { Id = 3, Kode = "K003", Nama = "Pepsodent 250gr", Guadang = "G003", Stok = 50 }
+        };
     }
 }
