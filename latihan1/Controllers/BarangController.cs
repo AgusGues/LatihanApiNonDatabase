eac4a56c9a7f668a34efc65b6c90167ad38fc2db
Agus <agusgus612@gmail.com>
2025-12-19 01:36:03 +0700

    g

---
+++ latihan1/Controllers/BarangController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using latihan1.Data;
+using latihan1.Models;
+
+namespace latihan1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarangController : ControllerBase
+    {
+       
+    }
+}
--- latihan1/Data/BarangData.cs
+++ latihan1/Data/BarangData.cs
@@ -1,14 +1,12 @@
 ﻿using latihan1.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace latihan1.Data
 {
     public static class BarangData
     {
-        public static List<Barang> ListBarang = new()
-        {
-            new Barang { Id = 1, Kode = "K001", Nama = "Sabun Lifebouy", Guadang= "G001", Stok= 30 },
-            new Barang { Id = 2, Kode = "K002", Nama = "Sunlight Jeruk Nipis Pencuci piring", Guadang = "G002", Stok = 12},
-            new Barang { Id = 3, Kode = "K003", Nama = "Pepsodent 250gr", Guadang = "G003", Stok = 50 }
-        };
+        
+
+       
     }
 }
