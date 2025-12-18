c67bac8155f05ad384484e64467673aa923c27db
Agus <agusgus612@gmail.com>
2025-12-19 01:00:57 +0700

    add isi model class barang

--- latihan1/Models/Barang.cs
+++ latihan1/Models/Barang.cs
@@ -2,5 +2,10 @@
 {
     public class Barang
     {
+        public int Id {  get; set; }
+        public string Kode { get; set; }
+        public string Nama { get; set; }
+        public string Guadang { get; set; }
+        public int Stok {  get; set; }
     }
 }
